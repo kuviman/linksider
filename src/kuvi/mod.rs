9885c7437b226183f16258810ff2b85b22f0d8c3
kuviman <kuviman@gmail.com>
2023-04-02 19:22:50 +0400

    crab

--- src/kuvi/mod.rs
+++ src/kuvi/mod.rs
@@ -2,6 +2,17 @@ use bevy::prelude::*;
 
 pub struct Stuff;
 
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle { ..default() });
+    commands.spawn(SpriteBundle {
+        transform: default(),
+        texture: asset_server.load("texture.png"),
+        ..default()
+    });
+}
+
 impl Plugin for Stuff {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup);
+    }
 }
