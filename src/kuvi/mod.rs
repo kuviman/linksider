c142324fde39f5001737a5b8f67ea2127d5c9861
kuviman <kuviman@gmail.com>
2023-04-02 22:13:47 +0400

    Separate playground bin

--- src/kuvi/mod.rs
+++ src/kuvi/mod.rs
@@ -2,17 +2,6 @@ use bevy::prelude::*;
 
 pub struct Stuff;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle { ..default() });
-    commands.spawn(SpriteBundle {
-        transform: default(),
-        texture: asset_server.load("texture.png"),
-        ..default()
-    });
-}
-
 impl Plugin for Stuff {
-    fn build(&self, app: &mut App) {
-        app.add_startup_system(setup);
-    }
+    fn build(&self, app: &mut App) {}
 }
