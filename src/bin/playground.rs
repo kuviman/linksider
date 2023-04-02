a184bf00f36d8b183da54f7c603c2d3f0ec9367d
kuviman <kuviman@gmail.com>
2023-04-02 23:35:18 +0400

    Fullscreen on release & full page web

--- src/bin/playground.rs
+++ src/bin/playground.rs
@@ -84,7 +84,22 @@ impl Plugin for Stuff {
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy Jam 3 🦀 Playground".into(),
+                mode: if cfg!(debug_assertions) {
+                    bevy::window::WindowMode::Windowed
+                } else {
+                    bevy::window::WindowMode::BorderlessFullscreen
+                },
+                // Tells wasm to resize the window according to the available canvas
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugin(Stuff)
         .run();
 }
