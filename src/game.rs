de76382e0bc8fde3f2f57ada648582a50302fd14
kuviman <kuviman@gmail.com>
2023-04-03 16:25:36 +0400

    Simple txt level

--- src/game.rs
+++ src/game.rs
@@ -8,8 +8,9 @@ struct Player;
 
 impl bevy::app::Plugin for Plugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup);
-        app.add_system(update);
+        app.add_startup_system(setup)
+            .add_system(update_player_input)
+            .add_system(update_camera);
     }
 }
 
@@ -24,12 +25,52 @@ fn setup(
         bundle.projection.scaling_mode = bevy::render::camera::ScalingMode::FixedVertical(10.0);
         bundle
     });
+    let map: Vec<Vec<char>> = include_str!("level.txt")
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+    let w = map.iter().map(|row| row.len()).max().unwrap();
+    let h = map.len();
+    let map = |x: usize, y: usize| map[h - 1 - y].get(x).copied().unwrap_or(' ');
+    let index = |x, y| (x + y * (w + 1)) as u32;
+    let mut trimesh_indices = Vec::new();
+    let mut player_location = None;
+    #[allow(clippy::needless_range_loop)]
+    for x in 0..w {
+        for y in 0..h {
+            match map(x, y) {
+                '#' => trimesh_indices.extend([
+                    [index(x, y), index(x + 1, y), index(x, y + 1)],
+                    [index(x + 1, y + 1), index(x, y + 1), index(x + 1, y)],
+                ]),
+                'L' => trimesh_indices.push([index(x, y), index(x + 1, y), index(x + 1, y + 1)]),
+                'R' => trimesh_indices.push([index(x, y), index(x + 1, y), index(x, y + 1)]),
+                'S' => {
+                    player_location = Some((x, y));
+                }
+                ' ' => {}
+                _ => unreachable!(),
+            }
+        }
+    }
+    commands.spawn(Collider::trimesh(
+        (0..h + 1)
+            .flat_map(|y| (0..w + 1).map(move |x| Vec2::new(x as _, y as _)))
+            .collect(),
+        trimesh_indices,
+    ));
+
+    let player_size = 0.7;
     commands.spawn((
         Player,
         SpriteBundle {
             sprite: Sprite {
-                custom_size: Some(Vec2::new(2.0, 2.0)),
+                custom_size: Some(Vec2::splat(player_size)),
                 ..default()
+            },
+            transform: {
+                let (x, y) = player_location.unwrap();
+                Transform::from_xyz(x as f32 + 0.5, y as f32 + 0.5, 0.0)
             },
             texture: asset_server.load("texture.png"),
             ..default()
@@ -37,17 +78,13 @@ fn setup(
         RigidBody::Dynamic,
         Velocity::zero(),
         Friction::new(1.5),
-        Collider::round_cuboid(1.0, 1.0, 0.1),
-        ColliderMassProperties::Density(0.1),
+        Collider::round_cuboid(player_size / 2.0, player_size / 2.0, player_size * 0.1),
+        ColliderMassProperties::Density(1.0),
         ExternalForce::default(),
-    ));
-    commands.spawn((
-        TransformBundle::from_transform(Transform::from_xyz(0.0, -2.0, 0.0)),
-        Collider::halfspace(Vec2::Y).unwrap(),
     ));
 }
 
-fn update(
+fn update_player_input(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(&Velocity, &mut ExternalForce), With<Player>>,
 ) {
@@ -66,4 +103,13 @@ fn update(
             force.torque = 0.0;
         }
     }
+}
+
+fn update_camera(
+    mut camera: Query<&mut Transform, (With<Camera2d>, Without<Player>)>,
+    player: Query<&Transform, With<Player>>,
+) {
+    let Some(mut camera) = camera.iter_mut().next() else { return };
+    let Some(player) = player.iter().next() else { return };
+    camera.translation = player.translation;
 }
