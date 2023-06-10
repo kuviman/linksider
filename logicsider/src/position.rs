c9494471ae53167d04464730922f9df6f3f05b0a
kuviman <kuviman@gmail.com>
2023-06-10 05:06:00 +0400

    Completely remove any references to ldtk

--- logicsider/src/position.rs
+++ logicsider/src/position.rs
@@ -7,26 +7,6 @@ pub struct Position {
 }
 
 impl Position {
-    pub fn from_ldtk_entity(entity: &ldtk_json::EntityInstance, down_angle: IntAngle) -> Self {
-        Self {
-            cell: vec2(entity.grid[0], -entity.grid[1]),
-            angle: entity
-                .field_instances
-                .iter()
-                .find(|field| field.identifier == "Side")
-                .map_or(IntAngle::DOWN, |field| {
-                    match field.value.as_str().expect("Side value not a string WTF") {
-                        "Down" => IntAngle::DOWN,
-                        "Right" => IntAngle::RIGHT,
-                        "Left" => IntAngle::LEFT,
-                        "Up" => IntAngle::UP,
-                        value => unreachable!("Unexpected side value {value:?}"),
-                    }
-                })
-                - IntAngle::DOWN
-                + down_angle,
-        }
-    }
     pub fn normalize(&self) -> Self {
         Self {
             cell: self.cell,
